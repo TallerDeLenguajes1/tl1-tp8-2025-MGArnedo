8c9a62e0d8dd3b8c07e6349382531bd39f752236
MGArnedo <mguaarnedo@gmail.com>
2025-06-12 02:12:15 -0300

    Ejercicio 1 terminado: Se agrego un menu principal

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -6,6 +6,50 @@ class Program
     {
         List<Tarea> tareasPendientes = new List<Tarea>();
         List<Tarea> tareasRealizadas = new List<Tarea>();
+        CrearListaAleatoria(tareasPendientes);
+        int opcion;
+        do
+        {
+            Console.WriteLine("===MENU PRINCIPAL===\nPresione 1 para mover tareas pendientes a realizadas\nPresione 2 para buscar tareas pendientes por descripcion\nPresione 3 para mostar la lista de tareas pendientes y realizadas\nPresione 0 para salir del programa");
+            Console.WriteLine("Elija un opcion: ");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    MoverTareasDeUnaListaAOtra(tareasPendientes, tareasRealizadas);
+                    break;
+                case 2:
+                    BuscarTareasPorDescripcionYMostrarlas(tareasPendientes);
+                    break;
+                case 3:
+                    MostrarLista(tareasPendientes, "Lista tareas PENDIENTES");
+                    MostrarLista(tareasRealizadas, "Lista tareas REALIZADAS");
+                    break;
+                case 0:
+                    Console.WriteLine("===FIN DEL PROGRAMA===");
+                    break;
+                default:
+                    Console.WriteLine("Entrada invalida: Elija un numero entre 1 y 3");
+                    break;
+            }
+        } while (opcion != 0);
+    }
+
+        
+        //3. Desarrolle una interfaz para mover las tareas pendientes a realizadas. La lista de tareas realizadas (por ejemplo, List<Tarea> tareasRealizadas) inicialmente estará vacía. 
+        
+
+        //4. Desarrolle una función para buscar tareas pendientes por descripción y mostrarla por consola. 
+        
+        
+        //5. Mostrar un listado de todas las tareas (pendientes y realizadas) 
+        
+        //6. Diseña un menú principal que permita al usuario acceder a cada una de las funcionalidades descritas. La interacción debe ser intuitiva (ej. "Presione 1 para...", "Ingrese el ID de la tarea:", etc.). 
+    
+
+    static void CrearListaAleatoria(List<Tarea> Lista1)
+    {
         Random random = new Random();
 
         int numeroDeTareas = 5;
@@ -14,20 +58,22 @@ class Program
         {
             string descripcion = $"Tarea {i + 1}";
             int duracion = random.Next(10, 101);
-            tareasPendientes.Add(new Tarea(descripcion, duracion));
+            Lista1.Add(new Tarea(descripcion, duracion));
         }
-        //3. Desarrolle una interfaz para mover las tareas pendientes a realizadas. La lista de tareas realizadas (por ejemplo, List<Tarea> tareasRealizadas) inicialmente estará vacía. 
+    }
+    static void MoverTareasDeUnaListaAOtra(List<Tarea> Lista1, List<Tarea> Lista2)
+    {
         while (true)
         {
             try
             {
                 Console.WriteLine("Ingrese el ID de la tarea pendiente para moverla a realizada: ");
                 int buscarId = int.Parse(Console.ReadLine());
-                Tarea tarea = tareasPendientes.Find(f => f.TareaID == buscarId);
+                Tarea tarea = Lista1.Find(f => f.TareaID == buscarId);
                 if (tarea != null)
                 {
-                    tareasPendientes.Remove(tarea);
-                    tareasRealizadas.Add(tarea);
+                    Lista1.Remove(tarea);
+                    Lista2.Add(tarea);
                 }
                 break;
             }
@@ -36,18 +82,19 @@ class Program
                 Console.WriteLine("Entrada invalida");
             }
         }
-        
-        //4. Desarrolle una función para buscar tareas pendientes por descripción y mostrarla por consola. 
+    }
+    static void BuscarTareasPorDescripcionYMostrarlas(List<Tarea> Lista1)
+    {
         while (true)
         {
             try
             {
                 Console.WriteLine("Ingrese la descripcion de las tareas a buscar: ");
                 string buscarDescripcion = Console.ReadLine().ToLower().Trim();
-                List<Tarea> tareas = tareasPendientes.FindAll(f => f.Descripcion.ToLower().Contains(buscarDescripcion));
+                List<Tarea> tareas = Lista1.FindAll(f => f.Descripcion.ToLower().Contains(buscarDescripcion));
                 if (tareas.Count > 0)
                 {
-                    MostrarTareas(tareas, "Lista de tareas pendientes que coinciden parcialmente con la descripcion");
+                    MostrarLista(tareas, "Lista de tareas pendientes que coinciden con la descripcion");
                 }
                 else
                 {
@@ -60,13 +107,8 @@ class Program
                 Console.WriteLine("Entrada invalida");
             }
         }
-        
-        //5. Mostrar un listado de todas las tareas (pendientes y realizadas) 
-        MostrarTareas(tareasPendientes, "Lista tareas PENDIENTES");
-        MostrarTareas(tareasRealizadas, "Lista tareas REALIZADAS");
-        //6. Diseña un menú principal que permita al usuario acceder a cada una de las funcionalidades descritas. La interacción debe ser intuitiva (ej. "Presione 1 para...", "Ingrese el ID de la tarea:", etc.). 
     }
-    static void MostrarTareas(List<Tarea> Lista1, string titulo)
+    static void MostrarLista(List<Tarea> Lista1, string titulo)
     {
         Console.WriteLine($"---{titulo}---");
         foreach (var tarea in Lista1)
--- ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+447248898116b6481be06608ce483a69796d6d2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b786f0c772c08ae642832e5803c2931faa69d95d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
